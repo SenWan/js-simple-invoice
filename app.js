2fb6bd59bed55723bbaba43551a90942678a99b5
U Sen Wan <senwan96@gmail.com>
2022-02-16 18:38:08 +0600

    initial coommit

---
+++ app.js
@@ -0,0 +1,37 @@
+const detailsButton = document.getElementById('detail-submit-btn');
+
+detailsButton.addEventListener('click', function() {
+    const buyerDetails = document.getElementById('buyer-details-input');
+    document.getElementById('buyer-info').innerText = buyerDetails.value;
+
+    buyerDetails.value = '';
+})
+
+const addProductBtn = document.getElementById('add-details-btn');
+
+addProductBtn.addEventListener('click', function() {
+    const infoTable = document.getElementById('info-table');
+    const itemName = document.getElementById('item-name-input');
+    const itemPrice = document.getElementById('item-price-input');
+    const itemQuantity = document.getElementById('item-quantity-input');
+
+    const totalPrice = parseInt(itemPrice) * parseInt(itemQuantity);
+
+    const tr = document.createElement('tr');
+    const th = document.createElement('th');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+
+    th.innerText = itemName.value;
+    td1.innerText = itemPrice.value;
+    td2.innerText = itemQuantity.value;
+    td3.innerText = totalPrice;
+
+    tr.appendChild(th);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    infoTable.appendChild(tr);
+
+})
